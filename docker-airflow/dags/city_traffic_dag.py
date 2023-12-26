8faf0ee7d825aa9b346044c8b81e038fe5d0388c
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-26 18:27:15 +0300

    modification

---
+++ docker-airflow/dags/city_traffic_dag.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.bash import BashOperator
+
+from datetime import datetime
+from random import randint
+# Define the data loading function
+def load_data_function():
+    import subprocess
+
+    # Path to your load_data.py script
+    load_data_script = "/home/habte/WEEK02/docker-airflow/my_traffic_project/load_data.py"
+
+    # Execute the script
+    subprocess.run(["python", load_data_script])
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'habtef',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Instantiate a DAG
+dag = DAG(
+    'city_traffic_dag',
+    default_args=default_args,
+    description='ELT Process for City Traffic Data',
+    schedule_interval=timedelta(days=1),
+)
+
+# Define Python and Bash operators for data loading and dbt run
+load_data_task = PythonOperator(
+    task_id='load_data',
+    python_callable=load_data_function,
+    dag=dag,
+)
+
+run_dbt_task = BashOperator(
+    task_id='run_dbt',
+    bash_command='docker-compose run dbt run --profiles-dir /root/.dbt',
+    dag=dag,
+)
+
+# Set up task dependencies
+load_data_task >> run_dbt_task
---
+++ docker-airflow/my_traffic_project/scripts/schemas/traject_schema.sql
@@ -0,0 +1,22 @@
+-- Create the  table
+CREATE TABLE IF NOT EXISTS raw_traffic_data(
+    id SERIAL PRIMARY KEY,
+    unique_id TEXT NOT NULL,
+    track_id INTEGER NOT NULL,
+    veh_type VARCHAR(255) NOT NULL,
+    traveled_distance FLOAT NOT NULL,
+    avg_speed FLOAT NOT NULL
+);
+
+-- Create the table
+CREATE TABLE IF NOT EXISTS traffic_data (
+    id SERIAL PRIMARY KEY,
+    unique_id TEXT NOT NULL,
+    lat FLOAT NOT NULL,
+    lon FLOAT NOT NULL,
+    speed FLOAT,
+    lon_acc FLOAT,
+    lat_acc FLOAT,
+    time FLOAT,
+    FOREIGN KEY (unique_id) REFERENCES raw_traffic_data(unique_id) ON DELETE CASCADE
+);
